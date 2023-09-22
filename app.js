3513fb2e57e364efb174aab46031f8e17b2a0da8
Saumitra <saumitraupadhyay28@gmail.com>
2023-09-22 18:21:26 +0530

    changed few things

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
+  });
 });
